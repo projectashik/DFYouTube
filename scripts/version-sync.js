d67448aaaf2c83da50e4e0b0d92861bf4e82993a
Ashik Chapagain <projectashik@gmail.com>
2025-03-05 12:10:06 +0545

    versioning script fix

--- scripts/version-sync.js
+++ scripts/version-sync.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Get the version from git tag
 const getGitTag = () => {
